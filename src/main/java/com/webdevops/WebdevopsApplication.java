fec25b8a5541d246cbbfeab09e553fe0514db0c0
Tarciso <tarcisoloiola@hotmail.com>
2018-03-24 13:56:06 -0400

    First Commit

---
+++ src/main/java/com/webdevops/WebdevopsApplication.java
@@ -0,0 +1,12 @@
+package com.webdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevopsApplication.class, args);
+	}
+}
